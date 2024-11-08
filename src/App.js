d7c55a34704399cfd0fb43ae50e516df57f60988
Özlem Çalışkan <ozlemcaliskan@sabanciuniv.edu>
2024-11-08 22:40:30 +0300

    Commit mesajı

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './MainLayout';
 import NotFoundPage from './pages/not_found_page';
 import CreateProductForm from './pages/create_product_page';
-import ProductList from './product/ProductList';
 import { productMockService } from './services/product_mock_service';
 import InventoryManagementPage from './pages/inventory_management_page';
+import ProductList from './product/ProductList';
 
 
 
->>>>>>> b6092c06ae4e5d30effeb9f402dee59827ec1aa9
 function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    // Generate and set products to state
     const generatedProducts = productMockService.generateProducts(10);
     setProducts(generatedProducts);
+    
+    // Print products to the console
     console.log('Generated Products:', generatedProducts);
   }, []);
+
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-<<<<<<< HEAD
-          <Route path="product" element={<ProductList products={products} />} />
-=======
->>>>>>> b6092c06ae4e5d30effeb9f402dee59827ec1aa9
           <Route path="product/create" element={<CreateProductForm />} />
           <Route path="product/edit" element={<InventoryManagementPage />} />
+          <Route path="product" element={<ProductList products={products} />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,31 +1,9 @@
 import {
     BarChartOutlined,
 } from '@ant-design/icons';
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';  
-
-const Routes = [
-  {
-    key: 'dashboard',
-    icon: <BarChartOutlined />,
-    label: 'Reports',
-    children: [
-      {
-        key: 'product',
-        label: <Link to="/product">Product</Link>,  
-      },
-      {
-        key: 'product/sell',
-        label: 'Sell',
-      },
-      {
-        key: 'product/buy',
-        label: 'Buy',     
-     },
-    ],
-  },
-=======
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Routes = [
     {
@@ -35,7 +13,7 @@ const Routes = [
         children: [
             {
                 key: 'product',
-                label: 'Product',
+                label: <Link to="/product">Product</Link>, 
             },
             {
                 key: 'product/sell',
@@ -47,7 +25,6 @@ const Routes = [
             },
         ],
     },
->>>>>>> b6092c06ae4e5d30effeb9f402dee59827ec1aa9
 ];
 
 export default Routes;
