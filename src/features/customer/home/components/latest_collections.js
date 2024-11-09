6cdc5d6ffaa922378835550900775b36e744033f
Gorkem Topcu <grkmtpcu@gmail.com>
2024-11-10 00:54:49 +0300

    Added: Home Page Product Lists

---
+++ src/features/customer/home/components/latest_collections.js
@@ -0,0 +1,27 @@
+import ProductCardList from "components/lists/product_card_list";
+import React, { useEffect, useState } from "react";
+import { productMockService } from "services/product_mock_service";
+
+const LatestCollections = () => {
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    const generatedCollections = productMockService.generateProducts(8);
+    setCollections(generatedCollections);
+  }, []);
+
+  return (
+    <div className="px-6 py-10 mx-auto" style={{ maxWidth: "1200px" }}>
+      <h1 className="text-3xl font-semibold text-center mb-4">
+        Latest Collections
+      </h1>
+      <p className="text-center text-gray-600 mb-8">
+        Explore our newest arrivals of clothing and accessories!
+      </p>
+
+      <ProductCardList products={collections} />
+    </div>
+  );
+};
+
+export default LatestCollections;
--- src/pages/customer/home.js
+++ src/pages/customer/home.js
@@ -1,10 +1,14 @@
+import BestSellers from "features/customer/home/components/best_sellers";
 import Hero from "features/customer/home/components/hero";
+import LatestCollections from "features/customer/home/components/latest_collections";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollections />
+      <BestSellers />
     </div>
   );
 };
