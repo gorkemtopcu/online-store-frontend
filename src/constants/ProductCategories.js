660fda7abb606a5677343e71e19e8352d4d38ecb
denizsmrc <semerci@sabanciuniv.edu>
2024-11-30 21:13:13 +0300

    Update ProductCategories.js
    updated categories for BookStore

--- src/constants/ProductCategories.js
+++ src/constants/ProductCategories.js
@@ -2,16 +2,16 @@ const Categories = [
   {
     name: "CATEGORIES",
     subcategories: [
-      "Fiction",            // Roman ve hikaye türleri
-      "Non-Fiction",        // Gerçek olaylar ve bilgi içerikli kitaplar
-      "Mystery",            // Gizem ve dedektif hikayeleri
-      "Science Fiction",    // Bilim kurgu
-      "Fantasy",            // Fantastik dünyalar ve hikayeler
-      "Biography",          // Biyografi ve anı
-      "Self-Help",          // Kişisel gelişim
-      "History",            // Tarih kitapları
-      "Young Adult",        // Genç yetişkin türü
-      "Children's Books"    // Çocuk kitapları
+                 
+      "Non-Fiction",        
+      "History",           
+      "Poetry",    
+      "Thriller",           
+      "Academic & Text Books",         
+      "Fantasy",          
+      "Science",            
+      "Mystery & Crime",        
+         
     ],
   }
 ];
